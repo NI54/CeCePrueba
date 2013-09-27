011880894fba6854719d9babf5a55a3106bd1698
NI54 <isco_s@hotmail.com>
2013-09-27 15:21:19 -0300

    asdasdasdasdasasdasdasdasdsd
    asdasdasdas

--- connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$connection= mysqli_connect("mysql.hostinger.com.ar","u389429516_admin","stalker","u389429516_test");
-
-?>
--- version.php
+++
@@ -1,7 +0,0 @@
-<?php
-$f=fopen('version.txt','r');
-$r= fread($f,100);
-
-echo $r;
-
-?>
