f7d8a39a0d016bbb5eae5b34db45e16b2518c985
NI54 <isco_s@hotmail.com>
2014-05-05 18:58:19 -0300

    asdas
    asdas

--- header.js
+++ header.js
@@ -17,6 +17,7 @@ $(window).on("orientationchange",function(){
     
 	$('.imgHeader').css({ width:'25em',height:'auto'});
 	$('.imgHeaderInvi').css({ width:'25em',height:'auto',visibility:'hidden'});
+	$('.menuAcostado').css({ position:'fixed',width:'auto',top:'22%',left:'-0.1%',height:'15em','z-index':'2'});
 	
   }
   else // Landscape
@@ -24,7 +25,7 @@ $(window).on("orientationchange",function(){
     //$( "imgHeader" ).attr( "style", "width:15em;height:auto;" );
 	$('.imgHeader').css({ width:'15em',height:'auto'});
 	$('.imgHeaderInvi').css({ width:'15em',height:'auto',visibility:'hidden'});
-    
+    $('.menuAcostado').css({ position:'fixed',width:'auto',top:'22%',left:'-0.1%',height:'10em','z-index':'2'});
   }
 });
 
